4753d11fbb621bfe3b8fa831a4ab201abf89ef50
HydraCoding <goekay.sahin@gmx.net>
2023-07-03 07:01:45 +0200

    show-projects hedaline ready to respons

--- src/app/show-projects/show-projects.component.ts
+++ src/app/show-projects/show-projects.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-show-projects',
   templateUrl: './show-projects.component.html',
-  styleUrls: ['./show-projects.component.scss']
+  styleUrls: ['./show-projects.component.scss'],
 })
 export class ShowProjectsComponent {
-
+  laptops: string[] = ['join.png', 'loco.png', 'crm.png'];
+  headlines: string[] = ['Join', 'El Pollo Loco', 'Ring of fire']
+  technologies: string[] = [
+    'JavaSctipt | HTML | CSS',
+    'JavaScript | HTML | CSS',
+    'Angular | Firebase'
+  ];
+  descriptions: string[] = ['Task manager inspired by Kanban System. Creat and organize task using drap and drop function, assing user and categories.', 'A simple Jump-and-Run game based on an object-oriented approach. Help Pepe against the Chickens', 'A very simple coustumer relationsship management system working with CRUD functionality'];
 }
