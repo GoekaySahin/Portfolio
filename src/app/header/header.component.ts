de77097498dceb0efdf4ef856870af04dbcc7db2
GökaySahin <goekay.sahin@gmx.net>
2023-07-24 14:58:44 +0200

    repair layouts

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AboutMeComponent } from '../about-me/about-me.component';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  mobileView: any;
+  aboutMe: AboutMeComponent;
+
+  constructor() {
+     this.aboutMe = new AboutMeComponent()
+     this.mobileView = this.aboutMe.actualSize;
+    
+  }
+
+
 
 }
