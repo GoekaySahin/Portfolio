8ed5fce7d8820629b026113f6022af7c0a6dc7e4
GökaySahin <goekay.sahin@gmx.net>
2023-08-10 17:34:05 +0200

    menu responsive

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,6 +9,8 @@ import { AboutMeComponent } from "../about-me/about-me.component";
 export class HeaderComponent {
   mobileView: any;
   aboutMe: AboutMeComponent;
+  openend = true;
+  close = false;
 
   @HostListener("window:resize", ["$event"])
   onResize(event: { target: { innerWidth: any } }) {
@@ -22,5 +24,55 @@ export class HeaderComponent {
   constructor() {
     this.aboutMe = new AboutMeComponent();
     this.mobileView = this.aboutMe.mobileView;
+  }
+
+  setButton(): void {
+    const buttonElement = document.getElementById(
+      "myButton"
+    ) as HTMLButtonElement;
+
+    buttonElement.classList.toggle("opened");
+    buttonElement.setAttribute(
+      "aria-expanded",
+      buttonElement.classList.contains("opened").toString()
+    );
+    this.openMobileLinks(buttonElement);
+  }
+
+  disableOveflow() {
+    document.body.style.overflow = "scroll";
+  }
+
+  openMobileLinks(buttonElement) {
+    if (buttonElement.classList.contains("opened")) {
+      this.scrollToTop();
+      this.bodyFix();
+      this.openend = false;
+      this.setCloseFalse();
+    } else {
+      this.setClose();
+      setTimeout(this.disableOveflow, 600);
+      setTimeout(() => this.setFalse(), 600);
+    }
+  }
+
+  setClose() {
+    this.close = true;
+  }
+
+  setCloseFalse() {
+    this.close = false;
+  }
+
+  setFalse() {
+    this.openend = true;
+  }
+
+  bodyFix() {
+    document.body.style.overflow = "hidden";
+  }
+
+  scrollToTop(): void {
+    window.scrollTo({ top: 0, behavior: "smooth" });
   }
 }
