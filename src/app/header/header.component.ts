24b97c3692ca246541cc8db6d5a73ab39a9aeeca
GökaySahin <goekay.sahin@gmx.net>
2023-08-10 13:16:47 +0200

    burgermenu added with dnone > 1024

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,27 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { AboutMeComponent } from '../about-me/about-me.component';
+import { Component, HostListener, OnInit } from "@angular/core";
+import { AboutMeComponent } from "../about-me/about-me.component";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"],
 })
-export class HeaderComponent  {
-
-
+export class HeaderComponent {
   mobileView: any;
   aboutMe: AboutMeComponent;
 
+  @HostListener("window:resize", ["$event"])
+  onResize(event: { target: { innerWidth: any } }) {
+    if (window.innerWidth < 1024) {
+      this.mobileView = true;
+    } else {
+      this.mobileView = false;
+    }
+  }
+
   constructor() {
-     this.aboutMe = new AboutMeComponent()
-     this.mobileView = this.aboutMe.actualSize;
-    
+    this.aboutMe = new AboutMeComponent();
+    this.mobileView = this.aboutMe.mobileView;
   }
-
-  showMenu() {
-    
-  }
-
-
-
 }
