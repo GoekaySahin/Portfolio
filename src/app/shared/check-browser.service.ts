4a5821b350e5ae88153e53c2727cf05cc8d1eb27
GökaySahin <goekay.sahin@gmx.net>
2023-10-26 23:15:41 +0200

    add safari layout setup

--- src/app/shared/check-browser.service.ts
+++ src/app/shared/check-browser.service.ts
@@ -5,10 +5,14 @@ import { Injectable } from "@angular/core";
 })
 export class CheckBrowserService {
   firefox = false;
+  safari = false;
 
   constructor() {
     if (window.navigator.userAgent.includes("Firefox")) {
       this.firefox = true;
+    }
+    if (window.navigator.userAgent.includes("Safari")) {
+      this.safari = true;
     }
   }
 }
